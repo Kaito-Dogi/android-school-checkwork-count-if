1216d8ff94aad74b13bcd96a247fac98f76d31ce
Kaito-Dogi <kendobu0405@gmail.com>
2022-01-05 21:17:24 +0900

    add: FizzBuzzの実装

--- app/src/main/java/app/doggy/checkworkcount_if/MainActivity.kt
+++ app/src/main/java/app/doggy/checkworkcount_if/MainActivity.kt
@@ -11,5 +11,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
+
+        // 整数を管理する変数
+        var count: Int = 0
+
+        // plus_buttonをクリックした時の処理
+        binding.plusButton.setOnClickListener {
+            // countの値を1増やす
+            count += 1
+
+            // countの値によって、count_textに表示する内容を変える
+            if (count % 15 == 0) {
+                binding.countText.text = "FizzBuzz"
+            } else if (count % 3 == 0) {
+                binding.countText.text = "Fizz"
+            } else if (count % 5 == 0) {
+                binding.countText.text = "Buzz"
+            } else {
+                binding.countText.text = count.toString()
+            }
+        }
     }
 }
