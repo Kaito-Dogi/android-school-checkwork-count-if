6591a153ad66fab012e6a6bc492154d35eeac306
Kaito-Dogi <kendobu0405@gmail.com>
2022-01-05 20:56:32 +0900

    add: View Bindingの設定

--- app/src/main/java/app/doggy/checkworkcount_if/MainActivity.kt
+++ app/src/main/java/app/doggy/checkworkcount_if/MainActivity.kt
@@ -2,10 +2,14 @@ package app.doggy.checkworkcount_if
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import app.doggy.checkworkcount_if.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // バインディングクラスの変数
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(this.root) }
     }
 }
